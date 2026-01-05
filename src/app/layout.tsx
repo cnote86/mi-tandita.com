5ef50661a88c0ad9fabb0c43fad3e71394e88d24
cnote86 <mc1986.99@gmail.com>
2026-01-05 00:14:31 -0800

    speed &analytics updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -205,6 +205,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
+        <SpeedInsights />
       </body>
     </html>
   );
